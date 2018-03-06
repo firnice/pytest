70f2a245518fd5478a9763e9fd103007954bb169
liyiliang <lyl120751@alibaba-inc.com>
2018-03-06 21:13:36 +0800

    测试

---
+++ TestThree/Code.py
@@ -0,0 +1,3 @@
+__author__ = 'firnice'
+
+print("你好");
---
+++ TestThree/Test1.py
@@ -0,0 +1,11 @@
+__author__ = 'firnice'
+
+counter = 100
+miles = 10.0
+name = "runoob"
+
+print(counter)
+print(miles)
+print(name)
+
+print(name[-1])
